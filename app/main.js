90d17787dde14d69b21819e0f41340de0deef0db
Alex Morozoff <a.j.morozoff@gmail.com>
2020-03-19 11:26:11 -0400

    added client integration

--- app/main.js
+++ app/main.js
@@ -4,9 +4,27 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Root from './components/Root';
 
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
 render(
-  <Provider store={store}>
-    <Root />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <Root />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('main')
 );
